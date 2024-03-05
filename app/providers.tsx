d0bb99ee6792778136cd829dd6b4744af81501ba
HiChon <59905927+chon26909@users.noreply.github.com>
2024-03-05 23:59:13 +0700

    add provider

---
+++ app/providers.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const Providers = ({ children }: Props) => {
+    return <>{children}</>;
+};
+
+export default Providers;
