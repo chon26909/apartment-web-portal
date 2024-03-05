018895b9170eae3b56626e2a08ec7d0c7a93c263
HiChon <59905927+chon26909@users.noreply.github.com>
2024-03-05 23:58:58 +0700

    add middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+    const user = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+
+    const { pathname } = request.nextUrl;
+
+    if (pathname.startsWith('/protected') && (!user || user.role !== 'admin')) {
+        return NextResponse.redirect(new URL('/auth/login', request.url));
+    }
+
+    return NextResponse.next();
+}
