62108eb64051e25bb55b2e3af0e431598e046431
HiChon <59905927+chon26909@users.noreply.github.com>
2024-03-08 23:39:48 +0700

    edit middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,30 @@
 import { getToken } from 'next-auth/jwt';
+import NextAuth from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
+import authConfig from '@/auth.config';
+import { publicRoutes } from './routes';
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req: any) => {
+    console.log(req);
+});
 
 export async function middleware(request: NextRequest) {
-    const user = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
+    // const isLoggedIn = !!request.auth;
+    // console.log('isLoggedIn', isLoggedIn);
 
     const { pathname } = request.nextUrl;
 
-    const isAuthRoute = ['/profile', '/protected'].includes(pathname);
+    const user = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
 
-    if (isAuthRoute && (!user || user.role !== 'admin')) {
-        return NextResponse.redirect(new URL('/auth/login', request.url));
-    }
+    const isPublicRoute = publicRoutes.includes(pathname);
+
+    //const isAuthRoute = ['/profile', '/protected'].includes(pathname);
+
+    // if (!user || user.role !== 'admin') {
+    //     return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
+    // }
 
     return NextResponse.next();
 }
