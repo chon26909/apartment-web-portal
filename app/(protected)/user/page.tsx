e409fa10a66fd8613f31ae2bb350a9f29ddf3471
HiChon <59905927+chon26909@users.noreply.github.com>
2024-03-12 20:29:04 +0700

    user page

---
+++ app/(protected)/user/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserAccountManagementPage = () => {
+    return (
+        <div>
+            <div> UserAccountManagementPage</div>
+        </div>
+    );
+};
+
+export default UserAccountManagementPage;
