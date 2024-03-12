ff65e25feda5d81d509dc0ac413c7ee9012a47d4
HiChon <59905927+chon26909@users.noreply.github.com>
2024-03-12 20:28:18 +0700

    edit app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Providers from './providers';
-import SessionProvider from './components/SessionProvider';
+import SessionProvider from '../components/SessionProvider';
 import { getServerSession } from 'next-auth';
 
 const inter = Inter({ subsets: ['latin'] });
