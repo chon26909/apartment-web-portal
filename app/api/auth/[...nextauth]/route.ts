b360fafe106aac81e397ec44e022ca8c16ebb223
HiChon <59905927+chon26909@users.noreply.github.com>
2024-03-26 22:03:48 +0700

    edit router

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,69 @@
-import NextAuth, { NextAuthOptions, Session, User } from 'next-auth';
+import NextAuth, { AuthOptions, Session, User } from 'next-auth';
 
 import { PrismaClient } from '@prisma/client';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { JWT } from 'next-auth/jwt';
 import { AdapterUser } from 'next-auth/adapters';
-import authConfig from '@/auth.config';
-
+// import authConfig from '@/auth.config';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
-export const authOptions: NextAuthOptions = {
-    ...authConfig,
+export const authOptions: AuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email', placeholder: '' },
+                password: { label: 'Password', type: 'password', placeholder: '' }
+            },
+            async authorize(credentials, req) {
+                if (!credentials) return null;
+                const user = await prisma.user.findUnique({
+                    where: { email: credentials.email }
+                });
+
+                if (!user) {
+                    return new Promise((resolve, reject) => {
+                        reject(new Error('Email not found'));
+                    });
+                }
+
+                if (user && (await bcrypt.compare(credentials.password, String(user.password)))) {
+                    const data = new Promise<User>((resolve) =>
+                        resolve({
+                            id: user.id.toString(),
+                            name: user.name,
+                            email: user.email,
+                            image: '',
+                            role: user.role
+                        })
+                    );
+
+                    return data;
+                }
+                return new Promise((resolve, reject) => {
+                    reject(new Error('Password incorrect'));
+                });
+            }
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            profile(profile) {
+                console.log('google profile', profile);
+
+                return {
+                    id: profile.sub,
+                    name: `${profile.given_name} ${profile.family_name}`,
+                    email: profile.email,
+                    image: profile.image,
+                    role: 'MEMBER'
+                };
+            }
+        })
+    ],
     adapter: PrismaAdapter(prisma),
     session: {
         strategy: 'jwt'
